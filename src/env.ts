dc975fc6e1c0a7fcae6e9beaf09909aae707f8f7
Zhansar <daid8002@gmail.com>
2025-05-11 11:09:24 +0500

    feat(better-auth): added better-auth drizzle adapter

--- src/env.ts
+++ src/env.ts
@@ -5,12 +5,19 @@ export const env = createEnv({
     BETTER_AUTH_URL: z.string().url(),
     BETTER_AUTH_SECRET: z.string(),
     DATABASE_URL: z.string().url(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
   },
-  client: {},
+  client: {
+    NEXT_PUBLIC_BETTER_AUTH_URL: z.string().url(),
+  },
   shared: {},
   runtimeEnv: {
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
   },
 });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,32 @@
 import { betterAuth } from "better-auth";
 import { BetterAuthOptions } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { env } from "../env";
+import { db } from "@/server/db";
+import { user } from "@/server/db/schema/auth-schema";
+import { account } from "@/server/db/schema/auth-schema";
+import { session } from "@/server/db/schema/auth-schema";
+
 export const auth = betterAuth<BetterAuthOptions>({
   appName: "Kerel",
   baseURL: env.BETTER_AUTH_URL,
   secret: env.BETTER_AUTH_SECRET,
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      user: user,
+      session: session,
+      account: account,
+    },
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      enabled: true,
+    },
+  },
 });

a746a8d86151ff1d3c8dae12a3ae28c2329636c3
Zhansar <daid8002@gmail.com>
2025-05-21 17:41:30 +0500

    fix api

--- frontend/src/server/index.ts
+++ frontend/src/server/index.ts
@@ -1,22 +1,4 @@
-import { generateText } from "ai";
-import { LanguageModelV1 } from "ai";
+export class FlashCardGenerator {}
 
-export class Server {
-  private readonly model: LanguageModelV1;
-  constructor(model: LanguageModelV1) {
-    this.model = model;
-  }
 
-  async getContentOfUserInterests(interests: string) {
-    const response = await generateText({
-      model: this.model,
-      prompt: interests,
-    });
-  }
-
-  async fetchContentFromWeb() {}
-
-  async splitContentIntoChunks(content: string) {}
-
-  async pushContentToDb() {}
-}
+export class 

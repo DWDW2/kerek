ab9a26c1b0d6b757d7236869000ee67fca665ec7
Zhansar <daid8002@gmail.com>
2025-06-09 13:19:29 +0500

    fix: fixed types of the handleKeyInput

--- web-socket/src/shared-canvas/SharedCanvasServer.ts
+++ web-socket/src/shared-canvas/SharedCanvasServer.ts
@@ -1,10 +1,25 @@
 import { WebSocketServer } from "ws";
 import { Server } from "http";
+import { SharedCanvasManager } from "./SharedCanvasManager";
 
 export class SharedCanvasServer {
   private wss: WebSocketServer;
-
+  private sharedCanvasManager: SharedCanvasManager;
   constructor(server: Server) {
     this.wss = new WebSocketServer({ server });
+    this.sharedCanvasManager = new SharedCanvasManager();
+    this.setupWebSocketHandlers();
+  }
+
+  private setupWebSocketHandlers() {
+    this.wss.on("connection", (ws, req) => {
+      console.log("New WebSocket connection");
+      const url = req.url;
+
+      if (url?.includes("/ws/shared-canvas")) {
+        this.sharedCanvasManager.handleConnection(ws);
+        return;
+      }
+    });
   }
 }

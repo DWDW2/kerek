2d04f1ffdfc90b7bb3fad564976e86d903f41a21
Zhansar <daid8002@gmail.com>
2025-06-14 17:47:24 +0500

    fix: websocket shapes broadcasting

--- frontend/src/components/groups/index.ts
+++ frontend/src/components/groups/index.ts
@@ -1,3 +1,4 @@
 export { CreateGroupDialog } from "./create-group-dialog";
 export { GroupCard } from "./group-card";
 export { GroupInviteHandler } from "./group-invite-handler";
+export { default as SharedCanvas } from "../shared-canvas/shared-canvas";
--- web-socket/src/shared-canvas/SharedCanvasService.ts
+++ web-socket/src/shared-canvas/SharedCanvasService.ts
@@ -1,65 +1,80 @@
 import WebSocket from "ws";
-import { BaseShape } from "./types";	
+import { BaseShape } from "./types";
 
 interface Message {
-	type: string;
+  type: string;
 }
 interface JoinRoom extends Message {
-	roomId: string;
-	userId: string;
+  roomId: string;
+  userId: string;
 }
 interface UpdateCanvas {
-	type: string;
-	roomId: string;
-	userId: string;
-	shapes: BaseShape[];
-
+  type: string;
+  roomId: string;
+  userId: string;
+  shapes: BaseShape[];
 }
 const rooms: Map<string, Map<string, any>> = new Map();
-const user_shapes: Map<string, any> = new Map()
+const user_shapes: Map<string, any> = new Map();
 export class SharedCanvasService {
+  private ws: WebSocket;
 
-	private ws: WebSocket; 
+  constructor(ws: WebSocket) {
+    this.ws = ws;
+  }
 
+  async handleMessage() {
+    this.ws.send(JSON.stringify({ type: "connected" }));
+    this.ws.on("message", (data) => {
+      console.log(data.toString());
+      const response: Message = JSON.parse(data.toString());
+      console.log(response);
+      if (response.type === "user_info") {
+        this.ws.send(
+          JSON.stringify({
+            type: "user_authenticated",
+          })
+        );
+        this.ws.on("message", (data) => {
+          const message: JoinRoom = JSON.parse(data.toString());
+          if (message.type === "join_room") {
+            rooms.set(
+              message.roomId,
+              user_shapes.set(message.userId, undefined)
+            );
+            this.ws.send(
+              JSON.stringify({
+                type: "room_joined",
+                userCount: rooms.get(message.roomId)?.size,
+              })
+            );
 
-	constructor(ws: WebSocket){
-		this.ws = ws;
-	}
-		
-
-
-	async handleMessage() {
-		this.ws.send(JSON.stringify({type: "connected"})) 
-		this.ws.on("message", (data) => {
-			const response: Message = JSON.parse(data.toString())
-
-			if(response.type === "user_info") {
-				this.ws.send(JSON.stringify({
-					type:"user_authenticated"
-				}))
-
-				this.ws.on("message", (data) => { 
-					const message: JoinRoom = JSON.parse(data.toString())
-					if(message.type === "join_room") {
-						rooms.set(message.roomId, user_shapes.set(message.userId, undefined)) 
-						this.ws.send(JSON.stringify({
-							type: "room_joined",
-							userCount: rooms.get(message.roomId)?.size
-						}))
-						this.ws.on("message", (data) => {
-
-							console.log(rooms)
-							const message: UpdateCanvas = JSON.parse(data.toString())
-							if(message.type === "canvas_update") {
-								rooms.get(message.roomId)?.set(message.userId, [...message.shapes])
-							}
-
-
-						})
-					}
-				})
-			}
-		})
-
-	}
+            this.ws.on("message", (data) => {
+              const message: UpdateCanvas = JSON.parse(data.toString());
+              if (message.type === "canvas_update") {
+                rooms
+                  .get(message.roomId)
+                  ?.set(message.userId, [...message.shapes]);
+                const room = rooms.get(message.roomId);
+                if (room) {
+                  for (const [userId, data] of room) {
+                    if (userId !== message.userId) {
+                      console.log(userId);
+                      this.ws.send(
+                        JSON.stringify({
+                          type: "canvas_update",
+                          userId: message.userId,
+                          shapes: message.shapes,
+                        })
+                      );
+                    }
+                  }
+                }
+              }
+            });
+          }
+        });
+      }
+    });
+  }
 }
